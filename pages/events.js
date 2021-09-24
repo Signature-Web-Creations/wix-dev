d1d1c598fcdd40a85040552b757f13a0f90b2aa8
Dominic Cartwright <dominic.cartwright07@gmail.com>
2021-09-24 12:20:02 -0400

    Add content to an element on a page

--- pages/events.js
+++ pages/events.js
@@ -1,5 +1,12 @@
 import { eventList } from 'backend/getEvents'
+import moment from 'moment'
 
-$w.onReady(function () {
-  eventList().then((events) => console.log(events))
+$w.onReady(async function () {
+  const limit = '5'
+  const events = await eventList(limit)
+  console.log(events)
+
+  $w('#wu247UpcomingEvents').forEachItem(($item, itemData, index) => {
+    $item('#eventMonth').text = moment(events[index].start).format('MMM')
+  })
 })
